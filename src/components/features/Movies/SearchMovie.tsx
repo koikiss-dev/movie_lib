1021c95d83aba2f1ae67ee9ea7791fbf5d026ba4
yako <duronj08@gmail.com>
2024-07-07 23:09:14 -0600

    more commit

---
+++ src/components/features/Movies/SearchMovie.tsx
@@ -0,0 +1,102 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Input } from "@/components/ui/input";
+import { useState, type ChangeEvent, type FC, useEffect } from "react";
+import axios from "axios";
+import { Skeleton } from "@/components/ui/skeleton";
+interface SearchMovieProps {}
+
+interface Movies {
+  name: string;
+  description: string;
+  url: string;
+  image: string;
+}
+
+const SearchMovie: FC<SearchMovieProps> = () => {
+  const abortController = new AbortController();
+  const [movies, setMovies] = useState<Movies[]>([]);
+  const [filteredMovies, setFilteredMovies] = useState<Movies[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchMovies = async () => {
+    const url = import.meta.env.API_PROD || "http://localhost:3000";
+    try {
+      const { data } = await axios.get(`${url}/peliculas`, {
+        signal: abortController.signal,
+      });
+      if (data) {
+        setMovies(data);
+        setFilteredMovies(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      setLoading(true);
+      throw new Error("Error fetching movies");
+    }
+  };
+
+  const filterMovies = (event: ChangeEvent<HTMLInputElement>) => {
+    const filterValue = event.target.value.toUpperCase();
+    const filtered = movies.filter((movie) =>
+      movie.name.toUpperCase().includes(filterValue)
+    );
+    setFilteredMovies(filtered);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+  return (
+    <>
+      <Input onChange={filterMovies} placeholder="Search a movie" />
+      <div className="">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mt-6">
+          {loading &&
+            Array.from({ length: 12 }).map((_, index) => (
+              <Skeleton key={index} className="flex-1 h-[300px]" />
+            ))}
+          {filteredMovies.map(({ name, image, description }, index) => (
+            <Dialog key={index}>
+              <DialogTrigger>
+                <Card className="flex-1 border-0 shadow-none cursor-pointer active:scale-95 transition-all">
+                  <CardContent className="p-0">
+                    <img
+                      width="200"
+                      height="300"
+                      className="w-full rounded-md"
+                      src={image}
+                      alt={name}
+                    />
+                  </CardContent>
+                  <CardHeader className="py-1 px-0 text-left">
+                    <CardTitle className="text-base truncate">{name}</CardTitle>
+                  </CardHeader>
+                </Card>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>{name}</DialogTitle>
+                  <DialogDescription>{description}</DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SearchMovie;
